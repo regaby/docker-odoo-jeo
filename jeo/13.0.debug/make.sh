ce3fe395afeaa7408e7546dee4f64031abca6f4b
Gabriela Rivero <regaby@gmail.com>
2020-07-11 19:22:12 -0300

    [UPD] odoo v13 jobiols/odoo-jeo -> regaby/odoo

--- jeo/13.0.debug/make.sh
+++ jeo/13.0.debug/make.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sd build --rm=true -t jobiols/odoo-jeo:13.0.debug ./
+sd build --rm=true -t regaby/odoo:13.0.debug ./
--- make.sh
+++ make.sh
@@ -3,43 +3,43 @@
 # Script par hacer Build de las imagenes solo local
 # Para hacer el build en docker pushear este repo en github
 
-cd jeo/11.0
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:11.0"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:11.0"
-fi
+# cd jeo/11.0
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:11.0"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:11.0"
+# fi
 
-cd ../11.0.debug
-if ! ./make.sh;
-then
-    echo "Failed jeo-11.0.debug"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:11.0.debug"
-fi
+# cd ../11.0.debug
+# if ! ./make.sh;
+# then
+#     echo "Failed jeo-11.0.debug"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:11.0.debug"
+# fi
 
-cd ../12.0
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:12.0"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:12.0"
-fi
+# cd ../12.0
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:12.0"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:12.0"
+# fi
 
-cd ../12.0.debug
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:12.0.debug"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:12.0.debug"
-fi
+# cd ../12.0.debug
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:12.0.debug"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:12.0.debug"
+# fi
 
-cd ../13.0
+cd jeo/13.0
 if ! ./make.sh;
 then
     echo "Failed odoo-jeo:13.0"
@@ -48,31 +48,31 @@ else
     echo "----------> Success odoo-jeo:13.0"
 fi
 
-cd ../13.0.debug
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:13.0.debug"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:13.0.debug"
-fi
+# cd ../13.0.debug
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:13.0.debug"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:13.0.debug"
+# fi
 
-cd ../14.0
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:14.0"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:14.0"
-fi
+# cd ../14.0
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:14.0"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:14.0"
+# fi
 
-cd ../14.0.debug
-if ! ./make.sh;
-then
-    echo "Failed odoo-jeo:14.0.debug"
-	exit 1
-else
-    echo "----------> Success odoo-jeo:14.0.debug"
-fi
+# cd ../14.0.debug
+# if ! ./make.sh;
+# then
+#     echo "Failed odoo-jeo:14.0.debug"
+# 	exit 1
+# else
+#     echo "----------> Success odoo-jeo:14.0.debug"
+# fi
 
 
