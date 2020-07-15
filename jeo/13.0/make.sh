73c8e9644a80535f785b8b992a4d43183b4a9c17
Gabriela Rivero <regaby@gmail.com>
2020-07-14 22:54:50 -0300

    [UPD] regaby/odoo -> regaby/odoo-ce

--- jeo/13.0/make.sh
+++ jeo/13.0/make.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sd build --rm=true -t regaby/odoo:13.0 ./
+sd build --rm=true -t regaby/odoo-ce:13.0 ./
--- make.sh
+++ make.sh
@@ -6,10 +6,10 @@
 # cd jeo/11.0
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:11.0"
+#     echo "Failed odoo-ce:11.0"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:11.0"
+#     echo "----------> Success odoo-ce:11.0"
 # fi
 
 # cd ../11.0.debug
@@ -18,61 +18,61 @@
 #     echo "Failed jeo-11.0.debug"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:11.0.debug"
+#     echo "----------> Success odoo-ce:11.0.debug"
 # fi
 
 # cd ../12.0
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:12.0"
+#     echo "Failed odoo-ce:12.0"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:12.0"
+#     echo "----------> Success odoo-ce:12.0"
 # fi
 
 # cd ../12.0.debug
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:12.0.debug"
+#     echo "Failed odoo-ce:12.0.debug"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:12.0.debug"
+#     echo "----------> Success odoo-ce:12.0.debug"
 # fi
 
 cd jeo/13.0
 if ! ./make.sh;
 then
-    echo "Failed odoo-jeo:13.0"
+    echo "Failed odoo-ce:13.0"
 	exit 1
 else
-    echo "----------> Success odoo-jeo:13.0"
+    echo "----------> Success odoo-ce:13.0"
 fi
 
 # cd ../13.0.debug
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:13.0.debug"
+#     echo "Failed odoo-ce:13.0.debug"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:13.0.debug"
+#     echo "----------> Success odoo-ce:13.0.debug"
 # fi
 
 # cd ../14.0
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:14.0"
+#     echo "Failed odoo-ce:14.0"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:14.0"
+#     echo "----------> Success odoo-ce:14.0"
 # fi
 
 # cd ../14.0.debug
 # if ! ./make.sh;
 # then
-#     echo "Failed odoo-jeo:14.0.debug"
+#     echo "Failed odoo-ce:14.0.debug"
 # 	exit 1
 # else
-#     echo "----------> Success odoo-jeo:14.0.debug"
+#     echo "----------> Success odoo-ce:14.0.debug"
 # fi
 
 
